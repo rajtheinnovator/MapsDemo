65531f74a09e203c686e1ada85e4a729f3036d11
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-09-14 23:07:32 +0530

    added custom marker icon

--- app/src/main/java/com/enpassio/mapsdemo/MainActivity.java
+++ app/src/main/java/com/enpassio/mapsdemo/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -67,12 +68,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         CameraPosition target = CameraPosition.builder().target(bengaluru).zoom(18).bearing(90).tilt(60).build();
 
 
-        MarkerOptions spurthy = new MarkerOptions().position(bengaluru).title("Swaraj");
+        MarkerOptions spurthy = new MarkerOptions().position(bengaluru).title("Spurthy").visible(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
 
-
-        //m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 1000, null);
+        MarkerOptions swaraj = new MarkerOptions().position(new LatLng(12.923519, 77.612030)).title("Swaraj").visible(true).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         m_map.addMarker(spurthy);
+        m_map.addMarker(swaraj);
+
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 1000, null);
+
     }
 }
